07385da09c044c1cc4acd9185725eed47a71ebdc
aumrcn <aumkung20728@gmail.com>
2021-09-04 15:36:28 +0700

    ez finished

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,3 +1,5 @@
+import { PixelPainterStore, setSelect } from "../stores/PixelPainterStore"
+
 type SelectColorButtonProps = {
   color: string;
 }
@@ -5,14 +7,19 @@ type SelectColorButtonProps = {
 const SelectColorButton = ({ color }: SelectColorButtonProps) => {
 
   //modify this function to highlight correctly
+  const state = PixelPainterStore.useState();
+
   const computeRingSize = () => {
-    return ""
-    return "ring-8 ring-green-400"
+    if (state.currentColor != color) {
+      return ""
+    } else {
+      return "ring-8 ring-green-400"
+    }
   }
 
   return (
     <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: color }} onClick={() => {setSelect(color)}}
     >
     </div>
   )
--- src/stores/PixelPainterStore.ts
+++ src/stores/PixelPainterStore.ts
@@ -2,7 +2,8 @@ import {Store} from 'pullstate'
 
 type PixelPainterStoreType = {
   //we save painted color as hex code (string) in 2D array
-  canvas: string[][] 
+  canvas: string[][]
+  currentColor : string
 }
 
 //return an (16 x 16) 2D array filled with "#FFFFFF"
@@ -17,6 +18,44 @@ const createEmptyCanvas = () => {
   return output
 }
 
+const arrColor : string[] = ["#000000","#804000",'#FE0000','#FE6A00','#FFD800','#00FF01','#FFFFFF','#01FFFF','#0094FE','#0026FF','#B100FE','#FF006E']
+
+export const randomCanvas = () => {
+  PixelPainterStore.update(e => {
+    
+    for (let i=0; i<16; i++){
+      e.canvas[i] = []
+      for (let j=0; j<16; j++){
+        e.canvas[i].push(arrColor[Math.floor(Math.random() * 12)])
+      }
+    }
+  })
+} 
+
 export const PixelPainterStore = new Store<PixelPainterStoreType>({
-  canvas: createEmptyCanvas()
+  canvas: createEmptyCanvas(),
+  currentColor: "#000000"
 })
+
+export const clearCanvas = () => {
+  PixelPainterStore.update(e => {
+    for (let i=0; i<16; i++){
+      e.canvas[i] = []
+      for (let j=0; j<16; j++){
+        e.canvas[i].push('#FFFFFF')
+      }
+    }
+  })
+}
+
+export const setSelect = (e : string) => {
+  PixelPainterStore.update(f => {
+    f.currentColor = e;
+  })
+}
+
+export const setCell = (arrCountI : number ,arrCountJ : number) => {
+  PixelPainterStore.update(e => {
+    e.canvas[arrCountI][arrCountJ] = e.currentColor;
+  })
+}
